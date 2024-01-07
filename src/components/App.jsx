caf27b6ea42dde5bf5c1aba676f5390d1b917bd4
DanilKulak <1998kylak@gmail.com>
2024-01-07 04:05:15 +0200

    new-commit-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import MyPhonebook from './MyPhonebook/PhonebookForm/MyPhonebook';
+
+class App extends Component {
+
+state = {
+  contacts: [],
+  name: "",
+}
+
+  render() {
+    return (
+      <>
+      <MyPhonebook/>
+      </>
+    )
+  }
+}
+export default App;
